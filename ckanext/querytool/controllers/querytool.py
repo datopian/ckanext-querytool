d18edf07d39062dd47cb3f454d70119760a38f2f
Dusko Bogdanovski <dusko.bogdanovski@keitaro.com>
2017-11-23 14:38:25 +0100

    Create barebone controller

---
+++ ckanext/querytool/controllers/querytool.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+import logging
+
+import ckan.lib.base as base
+
+log = logging.getLogger(__name__)
+
+class QueryTool(base.BaseController):
+
+    ## TODO implement functionality
+    def edit(self):
+        pass
+
+    ## TODO implement functionality
+    def show(self):
+        pass
