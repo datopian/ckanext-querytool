002bd4a49afeff018b6c7a858379d6952115d33d
Dusko Bogdanovski <dusko.bogdanovski@keitaro.com>
2017-11-23 15:21:02 +0100

    Create initial structure

--- ckanext/querytool/controllers/querytool.py
+++ ckanext/querytool/controllers/querytool.py
@@ -3,15 +3,53 @@
 import logging
 
 import ckan.lib.base as base
+import ckan.lib.helpers as h
+import ckan.logic as logic
+import ckan.model as model
+from ckan.common import config, request, c, _
 
 log = logging.getLogger(__name__)
 
+render = base.render
+abort = base.abort
+
+NotFound = logic.NotFound
+NotAuthorized = logic.NotAuthorized
+ValidationError = logic.ValidationError
+check_access = logic.check_access
+get_action = logic.get_action
+tuplize_dict = logic.tuplize_dict
+clean_dict = logic.clean_dict
+parse_params = logic.parse_params
+
+
 class QueryTool(base.BaseController):
 
-    ## TODO implement functionality
-    def edit(self):
-        pass
+    def _get_context(self):
 
-    ## TODO implement functionality
-    def show(self):
-        pass
+        context = {'model': model, 'session': model.Session,
+                   'user': c.user, 'for_view': True,
+                   'with_private': False}
+
+        return context
+
+
+    def edit(self):
+
+        context = self._get_context()
+
+        try:
+            ## TODO create, integrate authorization funtions
+            #check_access('squerytool_edit', context)
+            pass
+
+        except NotAuthorized:
+            abort(403, _('Not authorized to see this page'))
+
+        return render('querytool/admin/base.html',
+                      extra_vars={'msg': 'This is the Query Tool administration page'})
+
+    def index(self):
+
+        return render('querytool/public/base.html',
+                      extra_vars={'msg': 'This is the Query Tool public page and will be build from scratch'})
--- ckanext/querytool/plugin.py
+++ ckanext/querytool/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class QuerytoolPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes)
 
     # IConfigurer
 
@@ -11,3 +12,18 @@ class QuerytoolPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'querytool')
+
+    # IRoutes
+
+    def before_map(self, map):
+        ctrl = 'ckanext.querytool.controllers.querytool:QueryTool'
+
+        map.connect('querytool_edit', '/querytool/edit', controller=ctrl, action='edit')
+        map.connect('querytool_indext', '/querytool/index', controller=ctrl, action='index')
+
+        return map
+
+    def after_map(self, map):
+        return map
+
+
