25d3ef51249cad98c9cf40604250110b4b4115ed
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-11-29 01:24:15 +0100

    Add models

---
+++ ckanext/querytool/model.py
@@ -0,0 +1,80 @@
+import logging
+
+from sqlalchemy import Table, Column, Index, ForeignKey
+from sqlalchemy import types, func
+
+from sqlalchemy.engine.reflection import Inspector
+from ckan.model.meta import metadata, mapper, Session, engine
+from ckan.model.types import make_uuid
+from ckan.model.domain_object import DomainObject
+
+log = logging.getLogger(__name__)
+query_tool_table = None
+
+
+def setup():
+    print 'Vo seti['
+    if query_tool_table is None:
+        define_query_tool_table()
+        log.debug('Querytool table defined in memory.')
+    print 'stignav'
+    if not query_tool_table.exists():
+        print 'ovde'
+        query_tool_table.create()
+    else:
+        log.debug('Querytool table already exists.')
+    inspector = Inspector.from_engine(engine)
+
+    index_names =\
+        [index['name'] for index in
+            inspector.get_indexes('ckanext_querytool')]
+
+    if 'ckanext_querytool_id_idx' not in index_names:
+        print 'Podolu so index'
+        log.debug('Creating index for Querytool.')
+        Index('ckanext_querytool_id_idx',
+              query_tool_table.c.id).create()
+
+
+class CkanextQueryTool(DomainObject):
+    @classmethod
+    def get(self, **kwds):
+        '''Finds a single entity in the table.
+        '''
+
+        query = Session.query(self).autoflush(False)
+        query = query.filter_by(**kwds).first()
+        return query
+
+    @classmethod
+    def search(self, **kwds):
+        '''Finds entities in the table that satisfy certain criteria.
+        :param order: Order rows by specified column.
+        :type order: string
+        '''
+
+        query = Session.query(self).autoflush(False)
+        query = query.filter_by(**kwds)
+
+        return query.all()
+
+
+def define_query_tool_table():
+    global query_tool_table
+
+    query_tool_table = Table('ckanext_querytool', metadata,
+                             Column('id', types.UnicodeText,
+                                    primary_key=True,
+                                    default=make_uuid),
+                             Column('title',
+                                    types.UnicodeText,
+                                    nullable=False),
+                             Column('description', types.UnicodeText,
+                                    default=False),
+                             Index('ckanext_querytool_id_idx',
+                                  'id'))
+
+    mapper(
+        CkanextQueryTool,
+        query_tool_table
+    )
