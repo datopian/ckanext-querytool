50b1b042d7e8d9ff3012801d0e0b4b90c7876880
Dusko Bogdanovski <dusko.bogdanovski@keitaro.com>
2017-12-12 20:07:15 +0100

    Edit initial structure, add auth functions

---
+++ ckanext/querytool/helpers.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -
+try:
+    # CKAN 2.7 and later
+    from ckan.common import config
+except ImportError:
+    # CKAN 2.6 and earlier
+    from pylons import config
+import logging
+import ckan.model as m
+from ckan.common import c
+
+
+log = logging.getLogger(__name__)
+
+
+def _get_context():
+    return {
+        'model': m,
+        'session': m.Session,
+        'user': c.user or c.author,
+        'auth_user_obj': c.userobj
+    }
+
+
+def _get_functions(module_root, logic_functions={}):
+    '''
+     Helper function that scans extension
+     logic/auth dir for all logic/auth functions.
+     '''
+    for module_name in ['create', 'update', 'delete', 'get']:
+        module_path = '%s.%s' % (module_root, module_name,)
+
+        module = __import__(module_path)
+
+        for part in module_path.split('.')[1:]:
+            module = getattr(module, part)
+
+        for key, value in module.__dict__.items():
+            if not key.startswith('_') \
+                    and (hasattr(value, '__call__')
+                         and (value.__module__ == module_path)):
+                logic_functions[key] = value
+
+    return logic_functions
