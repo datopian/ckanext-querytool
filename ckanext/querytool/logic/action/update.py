9bff26f5ca53d3dd9254e34b5168b749e9f9175b
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-01-04 21:27:32 +0100

    Add update action for visualizations

--- ckanext/querytool/logic/action/update.py
+++ ckanext/querytool/logic/action/update.py
@@ -5,8 +5,8 @@ import ckan.lib.navl.dictization_functions as df
 from ckan.plugins import toolkit
 
 from ckanext.querytool.logic import schema
-from ckanext.querytool.model import CkanextQueryTool
-from ckanext.querytool.model import table_dictize
+from ckanext.querytool.model import CkanextQueryTool, table_dictize,\
+                                    CkanextQueryToolVisualizations
 
 check_access = logic.check_access
 
@@ -16,7 +16,9 @@ def querytool_update(context, data_dict):
         Create new query tool
     :param title
     :param description
-    :param name
+    :param dataset
+    :param filters
+    :param created
     '''
 
     check_access('querytool_update', context)
@@ -42,4 +44,36 @@ def querytool_update(context, data_dict):
     querytool.modified = datetime.datetime.utcnow()
     querytool.save()
     session.add(querytool)
+    session.commit()
+
+
+def querytool_visualizations_update(context, data_dict):
+    '''
+        Create new query tool visualizations
+    :param
+    :param
+    :param
+    '''
+
+    #check_access('querytool_update', context)
+
+    # we need the querytool name in the context for name validation
+    #context['querytool'] = data_dict['querytool']
+    session = context['session']
+    #data, errors = df.validate(data_dict, schema.querytool_schema(),
+     #                          context)
+
+    #if errors:
+    #    raise toolkit.ValidationError(errors)
+    print 'XXX', data_dict['name']
+    visualizations = \
+        CkanextQueryToolVisualizations.get(name=data_dict['name'])
+
+    if not visualizations:
+        visualizations = CkanextQueryToolVisualizations()
+
+    visualizations.name = data_dict['name']
+
+    visualizations.save()
+    session.add(visualizations)
     session.commit()
