6d9c549bc4d53328065ca4fd769ac62ee1d309fb
Goce Mitevski <goce.mitevski@keitaro.com>
2018-01-25 12:07:36 +0100

    Refactor code in public_query.js

--- ckanext/querytool/fanstatic/javascript/public_query.js
+++ ckanext/querytool/fanstatic/javascript/public_query.js
@@ -2,18 +2,13 @@
   'use strict';
 
   $(document).ready(function (e) {
+
     $('.btn-add-filter').on('click', function (e) {
-      $('.filters').append('<span class="fa fa-arrow-right"></span><div class="filter-group"><select class="filter-item filter-item-column"><option>&mdash; Select filter &mdash;</option><option>Column</option></select><select class="filter-item filter-item-value"><option>&mdash; Select value &mdash;</option><option>Value</option></select></div>');
+      $('.filters').append('<span class="fa fa-arrow-right"></span><div class="filter-group"><select class="filter-item filter-item-column"><option>&mdash; Select filter &mdash;</option><option>Column</option></select><select class="filter-item filter-item-value hidden"><option>&mdash; Select value &mdash;</option><option>Value</option></select></div>');
     });
 
     $('.filter-item-column').on('change', function (e) {
-      console.log('changed');
-      $(this).parent().find($('.filter-item-value')).show();
-    });
-
-    $('.btn-filter-toggle').click(function (e) {
-      $(this).toggleClass('on');
-      $('.public-query .controls').toggleClass('on');
+      $(this).closest('.filter-group').children('.filter-item-value').removeClass('hidden');
     });
 
   })
