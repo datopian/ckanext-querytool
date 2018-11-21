6f569544ce8278b307ec05b20f1e2c5b7bef5bf6
roll <eskarev@gmail.com>
2018-11-21 15:59:10 +0300

    Fixed styles building

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,28 @@ var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 // var sourcemaps = require('gulp-sourcemaps'); - Uncomment when developing
 
-// The default Gulp.js task
-gulp.task('default', ['less', 'watch']);
-
 // Rebuild CSS from LESS
 gulp.task('less', function () {
   return gulp.src('ckanext/querytool/fanstatic/less/*.less')
-    // .pipe(sourcemaps.init()) - Uncomment when developing
-    .pipe(less())
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(autoprefixer({
-      browsers: [
-        "last 5 versions",
-        "ie >= 11"
-      ]
-    }))
-    // .pipe(sourcemaps.write()) - Uncomment when developing
-    .pipe(gulp.dest('ckanext/querytool/fanstatic/css'));
+  // .pipe(sourcemaps.init()) - Uncomment when developing
+  .pipe(less())
+  .pipe(cleanCSS({
+    compatibility: 'ie8'
+  }))
+  .pipe(autoprefixer({
+    browsers: [
+      "last 5 versions",
+      "ie >= 11"
+    ]
+  }))
+  // .pipe(sourcemaps.write()) - Uncomment when developing
+  .pipe(gulp.dest('ckanext/querytool/fanstatic/css'));
 });
 
 // Watch for LESS file changes
 gulp.task('watch', function () {
-  gulp.watch(['ckanext/querytool/fanstatic/less/**/*.less'], ['less']);
+  gulp.watch(['ckanext/querytool/fanstatic/less/**/*.less'], gulp.parallel('less'));
 });
+
+// The default Gulp.js task
+gulp.task('default', gulp.parallel('less', 'watch'));
