9ff75f08ba13265c3d73a096831b5bcfea0f1803
roll <eskarev@gmail.com>
2018-11-25 18:25:35 +0300

    Downgrade less version

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 // var sourcemaps = require('gulp-sourcemaps'); - Uncomment when developing
 
+// The default Gulp.js task
+gulp.task('default', ['less', 'watch']);
+
 // Rebuild CSS from LESS
 gulp.task('less', function () {
   return gulp.src('ckanext/querytool/fanstatic/less/*.less')
@@ -24,8 +27,5 @@ gulp.task('less', function () {
 
 // Watch for LESS file changes
 gulp.task('watch', function () {
-  gulp.watch(['ckanext/querytool/fanstatic/less/**/*.less'], gulp.parallel('less'));
+  gulp.watch(['ckanext/querytool/fanstatic/less/**/*.less'], ['less']);
 });
-
-// The default Gulp.js task
-gulp.task('default', gulp.parallel('less', 'watch'));
