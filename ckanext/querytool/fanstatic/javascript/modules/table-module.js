db9592efd7f17aaab61ae994b4253fc9c133208d
roll <roll@users.noreply.github.com>
2018-12-10 15:39:23 +0300

    Table translation to Spanish, French and Portuguese (#318)
    * Implemented Y-axis based on Measure
    
    * Added tests
    
    * Updated position of Y-axis label
    
    * Added es/fr/pt_BR translations to dataTables.js
    
    * Added tests
    
    * Fixed typo in readme
    
    * Fixed typo in readme

--- ckanext/querytool/fanstatic/javascript/modules/table-module.js
+++ ckanext/querytool/fanstatic/javascript/modules/table-module.js
@@ -68,16 +68,113 @@ ckan.module('querytool-table', function() {
             }
             var locale = $('html').attr('lang');
             var languages = {
-                'zh_CN' : '//cdn.datatables.net/plug-ins/1.10.19/i18n/Chinese.json'
+              'es' : {
+                  "sProcessing":     "Procesando...",
+                  "sLengthMenu":     "Mostrar _MENU_ registros",
+                  "sZeroRecords":    "No se encontraron resultados",
+                  "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                  "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                  "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                  "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                  "sInfoPostFix":    "",
+                  "sSearch":         "Buscar:",
+                  "sUrl":            "",
+                  "sInfoThousands":  ",",
+                  "sLoadingRecords": "Cargando...",
+                  "oPaginate": {
+                      "sFirst":    "Primero",
+                      "sLast":     "Último",
+                      "sNext":     "Siguiente",
+                      "sPrevious": "Anterior"
+                  },
+                  "oAria": {
+                      "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                      "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                  }
+              },
+              'fr' : {
+                "sProcessing":     "Traitement en cours...",
+                "sSearch":         "Rechercher&nbsp;:",
+                "sLengthMenu":     "Afficher _MENU_ &eacute;l&eacute;ments",
+                "sInfo":           "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                "sInfoEmpty":      "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
+                "sInfoFiltered":   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                "sInfoPostFix":    "",
+                "sLoadingRecords": "Chargement en cours...",
+                "sZeroRecords":    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                "sEmptyTable":     "Aucune donn&eacute;e disponible dans le tableau",
+                "oPaginate": {
+                  "sFirst":      "Premier",
+                  "sPrevious":   "Pr&eacute;c&eacute;dent",
+                  "sNext":       "Suivant",
+                  "sLast":       "Dernier"
+                },
+                "oAria": {
+                  "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
+                  "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
+                },
+                "select": {
+                  "rows": {
+                    _: "%d lignes séléctionnées",
+                    0: "Aucune ligne séléctionnée",
+                    1: "1 ligne séléctionnée"
+                  }
+                }
+              },
+              'pt_BR' : {
+                  "sEmptyTable": "Nenhum registro encontrado",
+                  "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                  "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                  "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                  "sInfoPostFix": "",
+                  "sInfoThousands": ".",
+                  "sLengthMenu": "_MENU_ resultados por página",
+                  "sLoadingRecords": "Carregando...",
+                  "sProcessing": "Processando...",
+                  "sZeroRecords": "Nenhum registro encontrado",
+                  "sSearch": "Pesquisar",
+                  "oPaginate": {
+                      "sNext": "Próximo",
+                      "sPrevious": "Anterior",
+                      "sFirst": "Primeiro",
+                      "sLast": "Último"
+                  },
+                  "oAria": {
+                      "sSortAscending": ": Ordenar colunas de forma ascendente",
+                      "sSortDescending": ": Ordenar colunas de forma descendente"
+                  }
+              },
+              'zh_CN' : {
+                "sProcessing":   "处理中...",
+                "sLengthMenu":   "显示 _MENU_ 项结果",
+                "sZeroRecords":  "没有匹配结果",
+                "sInfo":         "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+                "sInfoEmpty":    "显示第 0 至 0 项结果，共 0 项",
+                "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+                "sInfoPostFix":  "",
+                "sSearch":       "搜索:",
+                "sUrl":          "",
+                "sEmptyTable":     "表中数据为空",
+                "sLoadingRecords": "载入中...",
+                "sInfoThousands":  ",",
+                "oPaginate": {
+                    "sFirst":    "首页",
+                    "sPrevious": "上页",
+                    "sNext":     "下页",
+                    "sLast":     "末页"
+                },
+                "oAria": {
+                    "sSortAscending":  ": 以升序排列此列",
+                    "sSortDescending": ": 以降序排列此列"
+                }
+              },
             };
             var sql_string = this.create_sql_string(main_value, y_axis);
             var dt_buttons_className = 'btn btn-default';
             var title = (this.options.table_title === true) ? '' : this.options.table_title;
             var dom_class = '<"dt-header' + id + '">';
             this.dataTable = $('#table-item-' + id).DataTable({
-                "language": {
-                    "url": languages[locale]
-                },
+                "language": languages[locale],
                 "processing": true,
                 "ajax": {
                     "url": api.url('querytool_get_resource_data', 'sql_string=' + sql_string),
