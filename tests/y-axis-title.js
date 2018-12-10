0cc89c798078ab22224d5b826d4886d444061266
roll <roll@users.noreply.github.com>
2018-12-10 15:32:04 +0300

    Dynamic Y axis title that changes according to selected measure (#317)
    * Implemented Y-axis based on Measure
    
    * Added tests
    
    * Updated position of Y-axis label

---
+++ tests/y-axis-title.js
@@ -0,0 +1,39 @@
+// It requires "Detailed Mortality by Cause" application (see staging)
+// Add "Title from config" as a title to the Y-axis of the 4th viz
+
+module.exports = {
+  afterEach: (client, done) => client.globals.report(client, done),
+
+  '[default measure] Heading and chart titles should be correct':
+    (client) => {
+      client
+        .url(`${client.launch_url}/querytool/public/detailed-mortality-by-cause`)
+        // Check heading
+        .waitForElementVisible('h1', 10000)
+        .assert.containsText('h1', 'Detailed Mortality by Cause')
+        // Set by measure selection
+        .waitForElementVisible('.chart:nth-child(1) .c3-axis-y-label', 10000)
+        .assert.containsText('.chart:nth-child(1) .c3-axis-y-label', 'Taxa de mortalidade')
+        // Set in the application config
+        .waitForElementVisible('.chart:nth-child(4) .c3-axis-y-label', 10000)
+        .assert.containsText('.chart:nth-child(4) .c3-axis-y-label', 'Title from config')
+        .end();
+    },
+
+  '[custom measure] Heading and chart titles should be correct':
+    (client) => {
+      client
+        .url(`${client.launch_url}/querytool/public/detailed-mortality-by-cause?detailed-mortality-by-cause_y_axis_column=Percentagem`)
+        // Check heading
+        .waitForElementVisible('h1', 10000)
+        .assert.containsText('h1', 'Detailed Mortality by Cause')
+        // Set by measure selection
+        .waitForElementVisible('.chart:nth-child(1) .c3-axis-y-label', 10000)
+        .assert.containsText('.chart:nth-child(1) .c3-axis-y-label', 'Percentagem')
+        // Set in the application config
+        .waitForElementVisible('.chart:nth-child(4) .c3-axis-y-label', 10000)
+        .assert.containsText('.chart:nth-child(4) .c3-axis-y-label', 'Title from config')
+        .end();
+    },
+
+};
