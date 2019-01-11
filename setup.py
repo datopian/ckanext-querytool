1f43b0e95c1cd3f932adc0588262099f873616b7
roll <eskarev@gmail.com>
2019-01-11 16:15:28 +0300

    v1.2a10

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.2a9',
+    version='1.2a10',
 
     description='''CKAN extension that will provide data quering with
                                                 pre configured set of rules''',
