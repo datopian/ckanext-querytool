d564fea608fc342ab66fc8d0541247b0de7cf633
roll <roll@users.noreply.github.com>
2019-01-13 16:17:06 +0300

    Implement "Y-axis from zero" option (#340)
    Implement "Y-axis from zero" option

--- ckanext/querytool/fanstatic/javascript/visualizations_settings.js
+++ ckanext/querytool/fanstatic/javascript/visualizations_settings.js
@@ -252,6 +252,9 @@
 
     function handleMultipleSelect () {
       $('[id*=chart_field_static_reference_columns_]')
+        // Skip already initialized
+        .not('.select2-container')
+        .not('.select2-offscreen')
         // Enable multiple select vidget
         .select2({
           placeholder: _('Click to select one or more'),
@@ -453,6 +456,7 @@
                     var selectDataFormat = item.find('[id*=chart_field_data_format_]');
                     var selectTickFormatAxisY = item.find('[id*=chart_field_y_ticks_format_]');
                     var inputLabelAxisY = item.find('[id*=chart_field_y_label_]');
+                    var inputYFromZero = item.find('[id*=chart_field_y_from_zero_]');
                     var inputPaddingTop = item.find('[id*=chart_field_padding_top_]');
                     var inputPaddingBottom = item.find('[id*=chart_field_padding_bottom_]');
                     var selectTickCount = item.find('[id*=chart_field_tick_count_]');
@@ -517,6 +521,9 @@
 
                     inputLabelAxisY.attr('id', 'chart_field_y_label_' + order);
                     inputLabelAxisY.attr('name', 'chart_field_y_label_' + order);
+
+                    inputYFromZero.attr('id', 'chart_field_y_from_zero_' + order);
+                    inputYFromZero.attr('name', 'chart_field_y_from_zero_' + order);
 
                     inputPaddingTop.attr('id', 'chart_field_padding_top_' + order);
                     inputPaddingTop.attr('name', 'chart_field_padding_top_' + order);
