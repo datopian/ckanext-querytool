a55c1b68c40beb342281ef9da761ec36afb41b8a
Tanvir Chahal <tnvr009@gmail.com>
2019-01-23 18:00:42 +0530

    Charts improvements (#342)
    * Updated accordion arrows
    
    * fixed overlapping labels

--- ckanext/querytool/fanstatic/javascript/visualizations_settings.js
+++ ckanext/querytool/fanstatic/javascript/visualizations_settings.js
@@ -769,3 +769,13 @@
     }
 
 })(ckan.i18n.ngettext, $);
+
+$(document).on('show','.accordion', function (e) {
+    //$('.accordion-heading i').toggleClass(' ');
+    $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+});
+
+$(document).on('hide','.accordion', function (e) {
+   $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+   //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+});
