21fbb61068670d4c4733f0ea3d1d5b6319d37055
Tanvir Chahal <tnvr009@gmail.com>
2019-02-19 17:40:24 +0530

    fixed indentation helpers.py

--- ckanext/querytool/helpers.py
+++ ckanext/querytool/helpers.py
@@ -177,13 +177,19 @@ def get_map_color_scheme():
     Get color schemes for displaying the maps
     :return:
     '''
-    colors = [{'value': '#feedde,#fdbe85,#fd8d3c,#e6550d,#a63603',
-               'text': _('Sequential')},
-							{'value': '#7b3294,#c2a5cf,#f7f7f7,#a6dba0,#008837',
-               'text': _('Diverging Green-Purple')},
-              {'value': '#d7191c,#fdae61,#ffffbf,#abdda4,#2b83ba',
-               'text': _('Diverging Blue-Red')}
-              ]
+    colors = [
+			{
+				'value': '#feedde,#fdbe85,#fd8d3c,#e6550d,#a63603',
+				'text': _('Sequential')
+			},
+			{
+				'value': '#d7191c,#fdae61,#ffffbf,#a6d96a,#1a9641',
+				'text': _('Diverging')},
+			{
+				'value': '#d7191c,#fdae61,#ffffbf,#a6d96a,#1a9641',
+				'text': _('Diverging Green-Purple')
+			}
+		]
 
     return colors
 
