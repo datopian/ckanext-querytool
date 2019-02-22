3c51f8d7ddc02c83def65b87c41bcff2d053829d
Tanvir Chahal <tnvr009@gmail.com>
2019-02-22 15:33:01 +0530

    Padding bottom for charts (#362)
    * fixed cut off x-labels
    
    * rolled back multi color spectrum

--- ckanext/querytool/fanstatic/javascript/modules/viz-preview.js
+++ ckanext/querytool/fanstatic/javascript/modules/viz-preview.js
@@ -236,7 +236,7 @@ ckan.module('querytool-viz-preview', function() {
                 },
                 padding: {
                     right: 50,
-                    bottom:14
+                    bottom:16
                 }
             };
 
@@ -260,7 +260,7 @@ ckan.module('querytool-viz-preview', function() {
                 padding: {
                     left: 0,
                     right: 0,
-                    bottom: 15,
+                    bottom: 18,
                     top:0
                 }
             }
