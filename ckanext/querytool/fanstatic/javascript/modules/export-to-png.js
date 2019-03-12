db70cdecb2bf0a8d710264156e7d8a850307a416
anuveyatsu <anuar.ustayev@gmail.com>
2019-03-12 12:37:28 +0600

    [c3 titles][s]: hide shapes outside y axis

--- ckanext/querytool/fanstatic/javascript/modules/export-to-png.js
+++ ckanext/querytool/fanstatic/javascript/modules/export-to-png.js
@@ -9,13 +9,19 @@ ckan.module('export-to-png', function($) {
         },
         exportChartToPng: function(className){
             //fix weird back fill
-            d3.select('.' + className).selectAll("path").attr("fill", "none");
+            d3.select('.' + className).selectAll('path').attr('fill', 'none');
             //fix no axes
-            d3.select('.' + className).selectAll("path.domain").attr("stroke", "black");
+            d3.select('.' + className).selectAll('path.domain').attr('stroke', 'black');
             //fix no tick
-            d3.select('.' + className).selectAll(".tick line").attr("stroke", "black");
+            d3.select('.' + className).selectAll('.tick line').attr('stroke', 'black');
             //fix reference lines
-            d3.select('.' + className).selectAll(".c3-ygrid-line.active").attr("display", "none");
+            d3.select('.' + className).selectAll('.c3-ygrid-line.active').attr('display', 'none');
+            //hide shapes outside y axis
+            d3.select('.' + className).selectAll('path.c3-shape')[0].forEach(function(shape, index) {
+              shape.pathSegList._list.forEach(function(item) {
+                item.x = item.x >= 0 ? item.x : 0;
+              });
+            });
             var svgElement = $('.' + className).find('svg')[0];
             saveSvgAsPng(svgElement, className + '.png', {backgroundColor: 'white'});
         }
