ee596c65e51976ccc534dfc62328e278a9cd23f3
anuveyatsu <anuar.ustayev@gmail.com>
2019-03-20 12:27:31 +0600

    [model][xs]: commit after table alteration

--- ckanext/querytool/model.py
+++ ckanext/querytool/model.py
@@ -32,6 +32,7 @@ def setup():
         log.debug('Querytool table already exists.')
         # add `owner_org` column if not exists:
         Session.execute('ALTER TABLE ckanext_querytool ADD COLUMN IF NOT EXISTS owner_org TEXT;')
+        Session.commit()
     inspector = Inspector.from_engine(engine)
 
     index_names =\
