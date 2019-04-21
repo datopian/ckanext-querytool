94be6c3b200e3fd6f47c0873056a0f631c2e33ef
anuveyatsu <anuar.ustayev@gmail.com>
2019-04-22 00:03:03 +0600

    [export][s]: bug fix when exporting graphs other than hbar and shbar

--- ckanext/querytool/fanstatic/javascript/modules/export-to-png.js
+++ ckanext/querytool/fanstatic/javascript/modules/export-to-png.js
@@ -2,12 +2,13 @@ ckan.module('export-to-png', function($) {
     return {
         initialize: function(){
             var id = this.options.id;
+            var chartType = this.options.type;
             this.el.click(function(e) {
-                e.preventDefault()
-                this.exportChartToPng(id)
+                e.preventDefault();
+                this.exportChartToPng(id, chartType);
             }.bind(this))
         },
-        exportChartToPng: function(className){
+        exportChartToPng: function(className, chartType){
             //fix weird back fill
             d3.select('.' + className).selectAll('path').attr('fill', 'none');
             //fix no axes
@@ -17,11 +18,13 @@ ckan.module('export-to-png', function($) {
             //fix reference lines
             d3.select('.' + className).selectAll('.c3-ygrid-line.active').attr('display', 'none');
             //hide shapes outside y axis
-            d3.select('.' + className).selectAll('path.c3-shape')[0].forEach(function(shape, index) {
-              shape.pathSegList._list.forEach(function(item) {
-                item.x = item.x >= 0 ? item.x : 0;
+            if (chartType === 'hbar' || chartType === 'shbar') {
+              d3.select('.' + className).selectAll('path.c3-shape')[0].forEach(function(shape, index) {
+                shape.pathSegList._list.forEach(function(item) {
+                  item.x = item.x >= 0 ? item.x : 0;
+                });
               });
-            });
+            }
             var svgElement = $('.' + className).find('svg')[0];
             saveSvgAsPng(svgElement, className + '.png', {backgroundColor: 'white'});
         }
