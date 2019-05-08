2cd3094cda75834a78423142dbd49ef0f4276fe1
Paul Walker <paul.walker@datopian.com>
2019-05-08 20:00:18 +0000

    Add remaining ext js via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,13 @@ module.exports = {
 	entry: {
     "public_query": `${assetPath}/public_query.js`,
     "visualizations_settings": `${assetPath}/visualizations_settings.js`,
+    "querytool_data": `${assetPath}/querytool_data.js`,
+    "querytool_list": `${assetPath}/querytool_list.js`,
     "modules/viz-preview": `${assetPath}/modules/viz-preview.js`,
-    "modules/table-module": `${assetPath}/modules/table-module.js`
+    "modules/table-module": `${assetPath}/modules/table-module.js`,
+    "modules/map-module": `${assetPath}/modules/map-module.js`,
+    "modules/tool-embed": `${assetPath}/modules/tool-embed.js`,
+    "modules/export-to-png": `${assetPath}/modules/export-to-png.js`
   },
 	output: {
 		path: path.resolve(__dirname, assetPath, 'dist')
