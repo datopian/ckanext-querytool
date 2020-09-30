a8cdc220d806324fee1d4343edab4a575dbd46c6
Tanvir Chahal <tnvr009@gmail.com>
2020-09-30 06:31:30 -0400

    Fix/groups design (#389)
    * changed layout of applicaitons page
    
    * added tabs for groups page
    
    * updated groups page
    
    * fixed applications design
    
    * groups admin fixes
    
    * design bugfixes
    
    * design bugfixes
    
    * added readmore functionality

---
+++ ckanext/querytool/fanstatic/javascript/vitals.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(document).ready(function(){
+        var maxLength = 300;
+        $(".show-read-more").each(function(){
+            var myStr = $(this).text();
+            if($.trim(myStr).length > maxLength){
+                var newStr = myStr.substring(0, maxLength);
+                var removedStr = myStr.substring(maxLength, $.trim(myStr).length);
+                $(this).empty().html(newStr);
+                $(this).append(' <a href="javascript:void(0);" class="read-more">read more...</a>');
+                $(this).append('<span class="more-text">' + removedStr + '</span>');
+            }
+        });
+        $(".read-more").click(function(){
+            $(this).siblings(".more-text").contents().unwrap();
+            $(this).remove();
+        });
+    });
+})
