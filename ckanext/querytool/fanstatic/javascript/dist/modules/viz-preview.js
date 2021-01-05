ac08f3b79be7a69da03532cade2afa30d49e84fc
Tanvir Chahal <tnvr009@gmail.com>
2021-01-05 17:20:01 +0530

    wip sequential colors

--- ckanext/querytool/fanstatic/javascript/dist/modules/viz-preview.js
+++ ckanext/querytool/fanstatic/javascript/dist/modules/viz-preview.js
@@ -2224,6 +2224,45 @@
                     });
                 }
 
+                
+                /**** Sequential Colors ******/
+                // Returns a single rgb color interpolation between given rgb color
+                function interpolateColor(color1, color2, factor) {
+                    if (arguments.length < 3) { 
+                        factor = 0.5; 
+                    }
+                    var result = color1.slice();
+                    for (var i = 0; i < 3; i++) {
+                        result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+                    }
+                    return result;
+                };
+
+                // My function to interpolate between two colors completely, returning an array
+                function interpolateColors(color1, color2, steps) {
+                    var stepFactor = 1 / (steps - 1),
+                        interpolatedColorArray = [];
+
+                    color1 = color1.match(/\d+/g).map(Number);
+                    color2 = color2.match(/\d+/g).map(Number);
+
+                    for(var i = 0; i < steps; i++) {
+                        var color_ = interpolateColor(color1, color2, stepFactor * i);
+
+                        var new_color_ = "rgba("+color_[0]+","+color_[1]+","+color_[2]+",1)";
+                        interpolatedColorArray.push(new_color_);
+                    }
+
+                    return interpolatedColorArray;
+                }
+
+                var sequential_colors = interpolateColors("rgb(255, 0, 0)", "rgb(239, 0, 255)", 7);
+                console.log(sequential_colors);
+
+                data[0].marker = {'color': sequential_colors};
+
+
+
                 //console.log(data);
                 console.log(O);
                 console.log(base_info); 
