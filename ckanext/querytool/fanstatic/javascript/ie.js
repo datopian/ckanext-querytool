b171e454fb458635c4df91afe300d2901a8a5cfc
Tanvir Chahal <tnvr009@gmail.com>
2021-09-28 17:31:03 -0400

    IE popup

--- ckanext/querytool/fanstatic/javascript/ie.js
+++ ckanext/querytool/fanstatic/javascript/ie.js
@@ -1,4 +1,10 @@
 if (window.document.documentMode) {
     // Do IE stuff
-    alert();
+    document.getElementById("iepopup").style.display = "block";
+    document.getElementById("ieshadow").style.display = "block"
+}
+
+function close_popup() {
+    document.getElementById("iepopup").style.display = "none";
+    document.getElementById("ieshadow").style.display = "none"
 }
