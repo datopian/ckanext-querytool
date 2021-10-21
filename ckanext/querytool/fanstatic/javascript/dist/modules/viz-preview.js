137c28c5315799c009bed8d1e3a089904936e9d6
Michael Polidori <contact@michaelpolidori.com>
2021-10-21 09:25:20 -0400

    Default ub and lb to true

--- ckanext/querytool/fanstatic/javascript/dist/modules/viz-preview.js
+++ ckanext/querytool/fanstatic/javascript/dist/modules/viz-preview.js
@@ -1419,7 +1419,7 @@
                 t = t.replace("+", "%2B");
                 var e = !0 === this.options.filter_name ? "" : this.options.filter_name,
                     n = !0 === this.options.filter_value ? "" : this.options.filter_value,
-                    i = !0 === this.options.y_axis ? true : this.options.y_axis,
+                    i = !0 === this.options.y_axis ? "" : this.options.y_axis,
                     r = !0 === this.options.static_reference_columns ? [] : this.options.static_reference_columns,
                     o = this.getStaticReferenceColumn(r, i);
                 !0 === this.options.category_name || this.options.category_name;
@@ -1467,7 +1467,7 @@
             "get_resource_datа": function(e) {
                 var n = !0 === this.options.category_name ? "" : this.options.category_name,
                     i = !0 === this.options.x_axis ? "" : this.options.x_axis,
-                    r = !0 === this.options.y_axis ? true : this.options.y_axis,
+                    r = !0 === this.options.y_axis ? "" : this.options.y_axis,
                     ub = !0 === this.options.upper_bounds ? "" : this.options.upper_bounds,
                     lb = !0 === this.options.lower_bounds ? "" : this.options.lower_bounds,
                     o = e.split("FROM")[1].split("WHERE")[0].split('"')[1],
