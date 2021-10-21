773d5c13e750f0c15edf42a6e6a03cad93db5d64
Michael Polidori <contact@michaelpolidori.com>
2021-10-21 19:10:39 -0400

    Change resource.config

--- ckanext/querytool/fanstatic/javascript/dist/modules/viz-preview.js
+++ ckanext/querytool/fanstatic/javascript/dist/modules/viz-preview.js
@@ -1437,8 +1437,8 @@
 
                 e && n && (t += ' AND ("' + tmp_filter_name + "\" = '" + tmp_filter_value + "')");
                 var sql,
-                    ub = this.options.upper_bounds ? true : this.options.upper_bounds,
-                    lb = this.options.lower_bounds ? true : this.options.lower_bounds;
+                    ub = this.options.upper_bounds,
+                    lb = this.options.lower_bounds;
                 if ([ub, lb].includes(i)) {
                   ub = 'true'
                   lb = 'true'
@@ -1461,15 +1461,14 @@
                     sql = 'SELECT "' + this.options.x_axis + '", SUM("' + this.options.y_axis + '") as "' + this.options.y_axis + '"' + t + ' GROUP BY "' + this.options.x_axis + '"';
                   }
                 }
-                console.log(`sql: ${sql}`)
                 return sql
             },
             "get_resource_datа": function(e) {
                 var n = !0 === this.options.category_name ? "" : this.options.category_name,
                     i = !0 === this.options.x_axis ? "" : this.options.x_axis,
                     r = !0 === this.options.y_axis ? "" : this.options.y_axis,
-                    ub = !0 === this.options.upper_bounds ? "true" : this.options.upper_bounds,
-                    lb = !0 === this.options.lower_bounds ? "true" : this.options.lower_bounds,
+                    ub = !0 === this.options.upper_bounds ? "" : this.options.upper_bounds,
+                    lb = !0 === this.options.lower_bounds ? "" : this.options.lower_bounds,
                     o = e.split("FROM")[1].split("WHERE")[0].split('"')[1],
                     a = this.options.chart_type,
                     s = !0 === this.options.filter_name ? "" : this.options.filter_name,
