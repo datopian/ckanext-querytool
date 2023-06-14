f5b85e8d84c11c49ead87a7c7855b3f2881ef9c3
Michael Polidori <michaelapolidori@gmail.com>
2023-06-14 11:48:18 -0400

    Add map hover translations

--- ckanext/querytool/fanstatic/javascript/modules/map-module.js
+++ ckanext/querytool/fanstatic/javascript/modules/map-module.js
@@ -320,7 +320,7 @@ ckan.module('querytool-map', function($) {
                     newContent += '<b>' + infoData.title + '</b><br/>'
                      + infoData.measure
                 } else {
-                    newContent += "Hover over the map for more info"
+                    newContent += translate("Hover over the map for more info");
                 }
 
                 this._div.innerHTML = newContent;
