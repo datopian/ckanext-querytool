580c7ef5bf32b9f39b102fac08f503d9cfbca3b1
Michael Polidori <contact@michaelpolidori.com>
2024-04-04 13:13:06 -0400

    Python 2to3 results and comment out code to get plugin to load

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='v2.1.5',
+    version='3.0.0',
 
     description='''CKAN extension that will provide data quering with
                                                 pre configured set of rules''',
@@ -46,7 +46,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 
 
