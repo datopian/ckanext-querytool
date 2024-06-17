9155066286714989b5dfcb5aecf2c4fbb7f499f6
Michael Polidori <contact@michaelpolidori.com>
2024-06-17 15:40:25 -0400

    Edit Visualizations (charts) (#634)
    * Init edit_visualizations
    
    * Fix chart options

--- ckanext/querytool/plugin.py
+++ ckanext/querytool/plugin.py
@@ -412,6 +412,8 @@ class QuerytoolPlugin(plugins.SingletonPlugin):
             "querytool_public_read": vs_get_actions.querytool_public_read,
             "querytool_get_chart_data": vs_get_actions.querytool_get_chart_data,
             "get_available_querytools": vs_get_actions.get_available_querytools,
+            "querytool_get_visualizations": vs_get_actions.querytool_get_visualizations,
+            "querytool_visualizations_update": vs_update_actions.querytool_visualizations_update,
         }
 
     # IConfigurable
